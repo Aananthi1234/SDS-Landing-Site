d2e83633445fc17231ecd74431a25f6394d75d2e
Aananthi1234 <anuskpk1208@gmail.com>
2024-06-15 04:55:32 +0100

    commited by Aananthi

--- src/Pages/Cources.jsx
+++ src/Pages/Cources.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import CoursesDetails from './Coursecomponent/CoursesDetails'
 
 function Courses() {
   return (
     <div>
+      <CoursesDetails/>
       <h1>Courses Offered</h1>
     </div>
   );
